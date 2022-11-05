bad8d6b46a4165287f6e678766e13641e5639d9b
Jessicaus <80499862+Jessicaus@users.noreply.github.com>
2022-11-05 14:09:46 +0800

    Update load_data_p.py

--- load_data_p.py
+++ load_data_p.py
@@ -44,10 +44,10 @@ for i in range(len(co2conc)):
     co2conc[i]=co2conc[i]-bar_y
 
 slope, intercept, r, p, std_err = stats.linregress(years, co2conc)
-
-
 def helplinear(years):
   return slope * years + intercept
+ def exp(x,a,b,c):
+    return a * np.exp(-b * x) + c
 
 #returns regression model of float array
 def linear_regression():
@@ -62,6 +62,14 @@ def poly_regression():
     myline = np.linspace(1, 100, 100)
     print(mymodel)
     model=mymodel(years)
+    return model
+   
+#returns regression model of float array
+def exp_regression():
+    popt, pcov = curve_fit(exp, years, co2conc,p0=(1, 1e-6, 1))
+    model= np.empty(len(years), dtype=object)
+    for i in range(len(years)):
+        model[i]=exp(years[i],*popt)
     return model
 
 #draws plot from model in form of float array
@@ -80,17 +88,22 @@ def residual(f_model):
         i+=1
     return resid
 
+def res_draw(model):
+    residuals=residual(model)
+    plt.scatter(years, residuals)
+    plt.show()
 
-polymodel=poly_regression()
 linearmodel = linear_regression()
+polymodel=poly_regression()
+expmodel=exp_regression()
 print(polymodel)
-# drawplot(polymodel)
-# drawplot(linearmodel)
+drawplot(linearmodel,"r-")
+drawplot(polymodel,"r-")
+drawplot(expmodel,"r-")
 
-residuals=residual(linearmodel)
-print(residuals)
-plt.scatter(years, residuals)
-plt.show()
+res_draw(linearmodel)
+res_draw(polymodel)
+res_draw(expmodel)
 
 years=sm.add_constant(years)
 results = sm.OLS(co2conc, years).fit()
