b45532e708192928a6d9e89292d7c4ebca57e79d
Jessicaus <80499862+Jessicaus@users.noreply.github.com>
2022-11-14 13:56:38 +0800

    Add files via upload

---
+++ Codes/2a3.py
@@ -0,0 +1,518 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[27]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[42]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+#!/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+from scipy import stats
+import scipy
+from scipy.stats import shapiro
+import statsmodels.api as sm
+from statsmodels.stats.weightstats import ttest_ind
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf  
+from statsmodels.tsa.stattools import adfuller
+from sklearn.model_selection import train_test_split
+from pmdarima.arima.utils import ndiffs  
+from scipy.optimize import curve_fit
+import pandas as pd
+#from varname import nameof
+import pmdarima as pm
+from pmdarima import auto_arima
+from statsmodels.tsa.seasonal import seasonal_decompose
+from pmdarima import auto_arima
+import warnings
+warnings.filterwarnings("ignore")
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+
+
+csfont = {'fontname':'Times New Roman'}
+plt.rcParams["font.family"] = "Times New Roman"
+
+# csv file name
+filename = "co2.csv "
+filename1= "temp.csv"
+# initializing the titles and rows list
+fields = []
+rows = []
+fields1 = []
+rows1 = []
+# reading csv file
+with open(filename, 'r') as csvfile:
+    # creating a csv reader object
+    csvreader = csv.reader(csvfile)
+     
+    # extracting field names through first row
+    fields = next(csvreader)
+ 
+    # extracting each data row one by one
+    for row in csvreader:
+        rows.append(row)
+ 
+    # get total number of rows
+    line_numbers = (csvreader.line_num)
+
+with open(filename1, 'r') as csvfile:
+    # creating a csv reader object
+    csvreader = csv.reader(csvfile)
+     
+    # extracting field names through first row
+    fields1 = next(csvreader)
+ 
+    # extracting each data row one by one
+    for row in csvreader:
+        rows1.append(row)
+ 
+    # get total number of rows
+    line_numbers = (csvreader.line_num)
+yearspre = []
+temperaturepre = []
+
+for item in rows1:
+    # yearspre.append(float(item[0])-1959)
+    yearspre.append(float(item[0])-1959)
+    print(item[0])
+for item in rows1:
+    temperaturepre.append(float(item[1]))
+bar_y=sum(temperaturepre)/len(temperaturepre)
+print(bar_y)
+for i in range(len(temperaturepre)):
+    temperaturepre[i]=temperaturepre[i]-bar_y
+
+print(yearspre)
+print(temperaturepre)
+yearsall=[]
+for i in range(-100,200):
+    yearsall.append(i)
+
+i=0
+help10 = int(len(yearspre)/10)
+
+years=[]
+temperature=[]
+yearstest=[]
+temperaturetest=[]
+yearslast=yearspre[46:]
+temperaturelast=temperaturepre[46:]
+years=yearspre[0:46]
+temperature=temperaturepre[0:46]
+i=0
+slope, intercept, r, p, std_err = stats.linregress(years, temperature)
+
+temperaturealldict={}
+
+i=0
+for year in yearspre:
+    temperaturealldict[year]=temperaturepre[i]
+    i+=1
+    
+def helplinear(years):
+    print("abcabcbcabcabcabca111111111111mmmmmmmmmm"+str(slope)+" "+str(years)+" "+str(intercept))
+    return slope * years + intercept
+  
+def exp(x,a,b,c):
+    print("abcabcbcabcabcabca444444444mmmmmm"+str(a)+" "+str(b)+" "+str(c))
+    return a * np.exp(-b * x) + c
+
+# def log(x,a,b,c):
+    # print("abcabcbcabcabcabca33333333333333mmmmmmm"+str(a)+" "+str(b)+" "+str(c))
+    return a+b*np.log(x) + c
+def log(x, a, b, c, d):
+    print("abcabcbcabcabcabca33333333333333mmmmmmm"+str(a)+" "+str(b)+" "+str(c)+""+str(d))
+    return a * np.exp(-b * (x - c)) + d
+#returns regression model of float array
+def linear_regression():
+    mymodel = list(map(helplinear, yearsall))
+    return mymodel
+
+#returns regression model of float array
+def exp_regression():
+    # popt, pcov = curve_fit(exp, years, temperature)
+    popt, pcov = curve_fit(exp, years, temperature,p0=(50, 0, 90))
+
+    model= np.empty(len(yearsall), dtype=object)
+    for i in range(len(yearsall)):
+        model[i]=exp(yearsall[i],*popt)
+    return model
+
+#returns regression model of float array
+def log_regression():
+    popt, pcov = curve_fit(log, years, temperature,p0=(50, 0, 90, 60), bounds=([0, 0, 90, 0], [1000, 0.1, 200, 200]))
+    model= np.empty(len(yearsall), dtype=object)
+    for i in range(len(yearsall)):
+        model[i]=log(yearsall[i],*popt)
+    return model
+
+#returns regression model of float array
+def poly_regression():
+    mymodel = np.poly1d(np.polyfit(years, temperature, 3))
+    print((mymodel))
+    model=mymodel(yearsall)
+    return model
+   
+def drawplot(model,x,y):
+    fig, ax = plt.subplots(figsize=(7, 5))
+    ax.grid()
+    ax.scatter(years, temperature, label='Training Data')
+    ax.plot(yearspre, model,c='k')
+    ax.scatter(yearslast, temperaturelast, c='r',label='Testing Data')
+    ax.set_xlabel("t")
+    ax.set_ylabel(r'$c-\bar{c}$')
+    ax.legend()
+    ax.title.set_text('{} Regression'.format(names[x*2+y]))
+
+def drawplotgrid(model,x,y):
+    ax[x,y].grid()
+    ax[x,y].scatter(years, temperature, label='Training Data')
+    print(len(yearsall))
+    print(len(model))
+    ax[x,y].plot(yearsall, model,c='k')
+    print(len(yearslast))
+    print(len(temperaturelast))
+    ax[x,y].scatter(yearslast, temperaturelast, c='r',label='Testing Data')
+    ax[x,y].set_xlabel("t")
+    ax[x,y].set_ylabel(r'$c-\bar{c}$')
+    ax[x,y].legend()
+    ax[x,y].title.set_text('{} Regression'.format(names[x*2+y]))
+plt.scatter(yearspre,temperaturepre)
+plt.show()
+def residualassess(model):
+    resid=0
+    i=0
+    f_model = {}
+    for year in yearspre:
+        f_model[year]=model[i]
+        i+=1
+    i=0
+    for year in yearslast:
+        residvalue = pow(temperaturealldict[year]-f_model[year],2)
+        resid=resid+residvalue
+        i+=1
+    return resid
+
+#returns residualtrains from model in form of float array
+def residualtrain(model):
+    resid = []
+    i=0
+    f_model = {}
+    for year in yearspre:
+        f_model[year]=model[i]
+        i+=1
+    i=0
+    for year in years:
+        residvalue = temperaturedict[year]-f_model[year]
+        resid.append(residvalue)
+        i+=1
+    return resid
+    
+def residualtest(model):
+    resid = []
+    i=0
+    f_model = {}
+    for year in yearspre:
+        f_model[year]=model[i]
+        i+=1
+    i=0
+    for year in yearslast:
+        residvalue = temperaturedict[year]-f_model[year]
+        resid.append(residvalue)
+        i+=1
+    return resid
+
+def drawresid(resids,name):
+    fig, ax = plt.subplots(figsize=(7, 5))
+    ax.grid()
+    ax.scatter(years, resids)
+    fig.savefig("11residualstrainingdata"+str(name)+".png")
+
+def drawresidtest(resids,name):
+    fig, ax = plt.subplots(figsize=(7, 5))
+    ax.grid()
+    ax.scatter(yearslast, resids)
+    ax.set_xlabel("t")
+    ax.set_ylabel("Deviations")
+    ax.title.set_text('Residual Plot')
+    fig.savefig("11residualstestdata"+str(name)+".png")
+
+def t_test(data_group1, data_group2):
+    a=np.array(data_group1)
+    b=np.array(data_group2)
+    out=str(ttest_ind(data_group1, data_group2))
+    return (out.split(" ")[1])#changed
+
+def f_test(group1, group2):
+    f = np.var(group1, ddof=1)/np.var(group2, ddof=1)
+    residualstestaa=np.array(group1)
+    residualstrainaa=np.array(group2)
+    nun = residualstestaa.size-1
+    dun = residualstrainaa.size-1
+    p_value = 1-scipy.stats.f.cdf(f, nun, dun)
+    return p_value#changed
+
+def shapiro_onlyp(data):
+    new=data.split(" ")[1]
+    new1=new.split("=")[1]
+    new2=new1.split(")")[0]
+    return new2#changed
+
+polymodel=poly_regression()
+linearmodel = linear_regression()
+logmodel = log_regression()
+#expmodel = exp_regression()
+#names=["Linear","Polynomial","Logarithmic","Exponential"]
+#models=[linearmodel,polymodel,logmodel,expmodel]
+names=["Linear","Polynomial","Logarithmic"]
+models=[linearmodel,polymodel,logmodel]
+
+# fig, ax = plt.subplots(2,2,figsize=(12, 7))
+# for i in range(2):
+    # for j in range(2):
+        # if((i*2+j)<3):
+            # drawplotgrid(models[i*2+j],i,j)
+# plt.show()
+
+# for i in range(2):
+    # for j in range(2):
+        # drawplot(models[i*2+j],i,j)
+#########################################################
+
+models=[]
+models.append(linearmodel)
+models.append(polymodel)
+models.append(logmodel)
+# models.append(expmodel)
+    
+i=0
+
+outputdata=[]
+
+# for model in models:
+    # residualstrain=residualtrain(model)
+    # residualstest=residualtest(model)
+    # modeldata=[]
+    # modeldata.append(residualassess(model))
+    # modeldata.append(t_test(residualstest,residualstrain))
+    # modeldata.append(shapiro_onlyp(str((shapiro(residualstest)))))
+    # modeldata.append(shapiro_onlyp(str((shapiro(residualstrain)))))
+    # modeldata.append(f_test(residualstest,residualstrain))
+    # outputdata.append(modeldata)
+    # i+=1
+
+from collections import OrderedDict
+
+# new_array=np.array(outputdata)
+# file = open("p_values.txt", "w+")
+# Saving the array in a text file
+# content = str(new_array)
+# file.write(content)
+# file.close()
+
+
+# In[54]:
+
+
+model=sm.OLS(temperature,sm.add_constant(years))
+p = model.fit().params
+print(p)
+print(model.fit().summary())
+
+
+# In[55]:
+
+
+yearss=np.square(years)
+dfy=[]
+for i in range(len(years)):
+    dfy.append([years[i],yearss[i]])
+model=sm.OLS(temperature, sm.add_constant(dfy))
+p = model.fit().params
+print(p)
+print(model.fit().summary())
+
+
+# In[67]:
+
+
+fig, ax = plt.subplots(figsize=(7, 5))
+ax.scatter(yearspre,temperaturepre)
+#ax.scatter(years_test,temps_test,label="Testing")
+#ax.plot(years_test,prediction,color='black',label="Predicted")
+ax.grid()
+ax.set_xlabel("Year")
+ax.set_ylabel("Land-Ocean Temperature (Degrees Celsius)")
+ax.title.set_text('Scatterplot of Land-Ocean Temperature')
+#ax.legend()
+plt.show()
+#print(type(model.predict(len(temps_test))))
+
+
+# In[79]:
+
+
+def residualtest(model,temperature):
+    resid = []; residsum=0
+    for i in range(len(temperature)):
+        r=abs(temperature[i]-model[i])
+        resid.append(r)
+        residsum+=r*r
+    return resid, residsum
+
+
+# In[76]:
+yearsy1=[]
+for i in range(len(yearspre)):
+    yearsy1.append([yearspre[i]])
+print(len(yearsy1))
+yearsy=yearsy1[:46]
+
+
+from sklearn.svm import SVR
+regressor = SVR(kernel="poly").fit(yearsy,temperature)
+prediction=regressor.predict(yearsy1)
+
+dct=dict(zip(yearspre,prediction))
+dct=OrderedDict(sorted(dct.items()))
+
+# In[77]:
+
+
+fig, ax = plt.subplots(figsize=(7, 5))
+ax.scatter(years,temperature)
+ax.plot(dct.keys(),dct.values(),color="green")
+ax.scatter(yearslast,temperaturelast,label="Testing")
+#ax.plot(years_test,prediction,color='black',label="Predicted")
+ax.grid()
+ax.set_xlabel("Year")
+ax.set_ylabel("Land-Ocean Temperature (Degrees Celsius)")
+ax.title.set_text('Scatterplot of Land-Ocean Temperature')
+#ax.legend()
+plt.show()
+#print(type(model.predict(len(temps_test))))
+
+
+# In[84]:
+
+
+regressor2 =SVR(kernel="rbf").fit(yearsy,temperature)
+prediction2=regressor2.predict(yearsy1)
+
+dct2=dict(zip(yearspre,prediction2))
+dct2=OrderedDict(sorted(dct2.items()))
+
+
+# In[85]:
+
+
+fig, ax = plt.subplots(figsize=(7, 5))
+ax.scatter(years,temperature)
+ax.plot(dct2.keys(),dct2.values(),color="green")
+ax.scatter(yearslast,temperaturelast,label="Testing")
+#ax.plot(years_test,prediction,color='black',label="Predicted")
+ax.grid()
+ax.set_xlabel("Year")
+ax.set_ylabel("Land-Ocean Temperature (Degrees Celsius)")
+ax.title.set_text('Scatterplot of Land-Ocean Temperature')
+#ax.legend()
+plt.show()
+#print(type(model.predict(len(temps_test))))
+
+regressor3 =SVR(kernel="linear").fit(yearsy,temperature)
+prediction3=regressor3.predict(yearsy1)
+
+dct3=dict(zip(yearspre,prediction3))
+dct3=OrderedDict(sorted(dct3.items()))
+
+fig, ax = plt.subplots(figsize=(7, 5))
+ax.scatter(years,temperature)
+ax.plot(dct3.keys(),dct3.values(),color="green")
+ax.scatter(yearslast,temperaturelast,label="Testing")
+#ax.plot(years_test,prediction,color='black',label="Predicted")
+ax.grid()
+ax.set_xlabel("Year")
+ax.set_ylabel("Land-Ocean Temperature (Degrees Celsius)")
+ax.title.set_text('Scatterplot of Land-Ocean Temperature')
+#ax.legend()
+plt.show()
+#print(type(model.predict(len(temps_test))))
+
+
+# In[81]:
+
+
+#SSM for SVR Poly Model
+resid,residsum=residualtest(prediction[46:],temperaturelast)
+print(residsum)
+
+
+# In[86]:
+
+
+#SSM for SVR RBF Model
+resid,residsum=residualtest(prediction2[46:],temperaturelast)
+print(residsum)
+
+#SSM for SVR Linear Model
+resid,residsum=residualtest(prediction3[46:],temperaturelast)
+print(residsum)
+
+# In[83]:
+
+#SSM For Linear Model
+resid,residsum=residualtest(linearmodel[46:],temperaturelast)
+#print(len(resid))
+#print(resid)
+print(residsum)
+
+
+# In[16]:
+
+
+def func2(x,y):
+    return p[2] * y + p[1] * x + p[0]
+
+
+# In[21]:
+
+
+modelt=list(map(func2,years,np.square(years)))
+modeltr=list(map(func2,yearslast,np.square(yearslast)))
+print(modeltr)
+
+
+# In[22]:
+
+
+resid,residsum=residualtest(modeltr,temperaturelast)
+print(residsum)
+
+
+# In[23]:
+
+
+fig, ax = plt.subplots(figsize=(7, 5))
+ax.scatter(years,temperature)
+ax.plot(years,modelt,color="green")
+ax.plot(yearslast,modeltr,color="red")
+ax.scatter(yearslast,temperaturelast,label="Testing")
+#ax.plot(years_test,prediction,color='black',label="Predicted")
+ax.grid()
+ax.set_xlabel("Year")
+ax.set_ylabel("Land-Ocean Temperature (Degrees Celsius)")
+ax.title.set_text('Scatterplot of Land-Ocean Temperature')
+#ax.legend()
+plt.show()
+#print(type(model.predict(len(temps_test))))
+
---
+++ Codes/2a4.py
@@ -0,0 +1,279 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+#!/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import csv
+from scipy import stats
+import statsmodels.api as sm
+from scipy.optimize import curve_fit
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf  
+from statsmodels.tsa.stattools import adfuller
+from sklearn.model_selection import train_test_split
+from pmdarima.arima.utils import ndiffs  
+# Import the library
+import pmdarima as pm
+from pmdarima import auto_arima
+# Fit a SARIMAX(0, 1, 1)x(2, 1, 1, 12) on the training set
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.tsa.arima_model import ARIMA 
+from statsmodels.tsa.seasonal import seasonal_decompose
+from sklearn.svm import SVR
+from collections import OrderedDict
+from sklearn.preprocessing import StandardScaler
+
+
+# In[2]:
+
+
+csfont = {'fontname':'Times New Roman'}
+plt.rcParams["font.family"] = "Times New Roman"
+# csv file name
+filename = "co2.csv"
+filename2 = "temp.csv"
+
+
+# In[3]:
+
+
+# initializing the titles and rows list
+fields = []
+rows = []
+ 
+# reading csv file
+with open(filename, 'r') as csvfile:
+    # creating a csv reader object
+    csvreader = csv.reader(csvfile)
+     
+    # extracting field names through first row
+    fields = next(csvreader)
+ 
+    # extracting each data row one by one
+    for row in csvreader:
+        rows.append(row)
+ 
+    # get total number of rows
+    line_numbers = (csvreader.line_num)
+    print(line_numbers)
+
+
+# In[4]:
+
+
+years=[]
+co2conc=[]
+for item in rows:
+    years.append(float(item[0])-1959)
+for item in rows:
+    co2conc.append(float(item[1]))
+years=np.array(years)
+co2conc=np.array(co2conc)
+
+
+# In[5]:
+
+
+# initializing the titles and rows list
+fields = []
+rows = []
+ 
+# reading csv file
+with open(filename2, 'r') as csvfile:
+    # creating a csv reader object
+    csvreader = csv.reader(csvfile)
+     
+    # extracting field names through first row
+    fields = next(csvreader)
+ 
+    # extracting each data row one by one
+    for row in csvreader:
+        rows.append(row)
+ 
+    # get total number of rows
+    line_numbers = (csvreader.line_num)
+    print(line_numbers)
+
+
+# In[6]:
+
+
+temps=[]
+for item in rows:
+    temps.append(float(item[1]))
+temps=temps[1:]
+temps=np.array(temps)
+
+
+# In[7]:
+
+
+#sc_X = StandardScaler()
+#sc_y = StandardScaler()
+#yearssc = sc_X.fit_transform(years[:, np.newaxis])
+#tempssc = sc_y.fit_transform(temps[:, np.newaxis])
+#yearssc=yearssc.flatten()
+#tempssc=tempssc.flatten()
+#print(yearssc)
+#print(tempssc)
+
+
+# In[8]:
+
+
+yearsy=[]
+for i in range(len(years)):
+    yearsy.append([years[i]])
+
+
+# In[9]:
+
+
+msk=int(len(temps)*0.75)
+years_train=yearsy[:msk]
+years_test=yearsy[msk:]
+temps_train=temps[:msk]
+temps_test=temps[msk:]
+#co2conc_train=co2conc[:msk]
+#co2conc_test=co2conc[msk:]
+print(temps_train)
+print(years_train)
+
+
+# In[10]:
+
+
+from sklearn.svm import SVR
+regressor = SVR(kernel="poly").fit(years_train,temps_train)
+prediction=regressor.predict(yearsy)
+
+dct=dict(zip(years,prediction))
+dct=OrderedDict(sorted(dct.items()))
+
+
+# In[11]:
+
+
+fig, ax = plt.subplots(figsize=(7, 5))
+ax.scatter(years_train,temps_train)
+ax.plot(dct.keys(),dct.values(),color="green")
+ax.scatter(years_test,temps_test,label="Testing")
+#ax.plot(years_test,prediction,color='black',label="Predicted")
+ax.grid()
+ax.set_xlabel("Year")
+ax.set_ylabel("Land-Ocean Temperature (Degrees Celsius)")
+ax.title.set_text('Scatterplot of Land-Ocean Temperature')
+#ax.legend()
+plt.show()
+#print(type(model.predict(len(temps_test))))
+
+
+# In[12]:
+
+
+regressor2 =SVR(kernel="rbf").fit(years_train,temps_train)
+prediction2=regressor2.predict(yearsy)
+
+dct2=dict(zip(years,prediction2))
+dct2=OrderedDict(sorted(dct2.items()))
+
+
+# In[85]:
+
+
+fig, ax = plt.subplots(figsize=(7, 5))
+ax.scatter(years_train,temps_train)
+ax.plot(dct2.keys(),dct2.values(),color="green")
+ax.scatter(years_test,temps_test,label="Testing")
+#ax.plot(years_test,prediction,color='black',label="Predicted")
+ax.grid()
+ax.set_xlabel("Year")
+ax.set_ylabel("Land-Ocean Temperature (Degrees Celsius)")
+ax.title.set_text('Scatterplot of Land-Ocean Temperature')
+#ax.legend()
+plt.show()
+#print(type(model.predict(len(temps_test))))
+
+
+# In[13]:
+
+
+def residualtest(model,temperature):
+    resid = []; residsum=0
+    for i in range(len(temperature)):
+        r=abs(temperature[i]-model[i])
+        resid.append(r)
+        residsum+=r*r
+    return resid, residsum
+
+
+# In[14]:
+
+
+#SSM for SVR Poly Model
+resid,residsum=residualtest(prediction[46:],temps_test)
+print(residsum)
+
+
+# In[86]:
+
+#SSM for SVR RBF Model
+resid,residsum=residualtest(prediction2[46:],temps_test)
+print(residsum)
+
+
+# In[15]:
+
+
+model=sm.OLS(temps_train, sm.add_constant(years_train))
+p= model.fit().params
+print(p)
+print(model.fit().summary())
+
+
+# In[16]:
+
+
+def func(x):
+    return p[1] * x + p[0]
+model=list(map(func,yearssc))
+model_train=model[:msk]
+model_test=model[msk:]
+
+
+# In[ ]:
+
+
+fig, ax = plt.subplots(figsize=(7, 5))
+ax.grid()
+ax.scatter(years_train,temps_train, label='Training Data')
+ax.plot(years_train, model_train,c='k')
+ax.plot(years_test, model_test,color='gray')
+ax.scatter(years_test,temps_test, c='r',label='Testing Data')
+ax.set_xlabel("Test Case")
+ax.set_ylabel("Predicted CO2 Concentration")
+ax.legend()
+ax.title.set_text("Linear Results")
+plt.plot()
+
+
+# In[ ]:
+
+
+resid,residsum=residualtest(model_test,temps_test)
+print(residsum)
+
+
+# In[ ]:
+
+
+
+
